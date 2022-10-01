987dad3190adda4a8f9b5d77cd541b3dcbb832f2
Ibrahim Velizade <ivalizada14@gmail.com>
2022-10-01 16:00:52 +0300

    second

--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
-        if((monthNumber>=0&&monthNumber <= 2) || monthNumber==12)
+        if((monthNumber>=1&&monthNumber <= 2) || monthNumber==12)
             System.out.println("Winter");
         else if(monthNumber>=3 && monthNumber<=5)
             System.out.println("Spring");
